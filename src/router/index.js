bc0429f54d0af6c35fb77ed1a52be009de6c5244
Lucas <r.lucasmoretto@gmail.com>
2022-06-04 12:47:55 -0300

    change routes

--- src/router/index.js
+++ src/router/index.js
@@ -17,10 +17,11 @@ const router = createRouter({
     {
       path: '/wallet',
       name: 'wallet',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/WalletView.vue')
+    },
+    {
+      path: '/:notFound(.*)',
+      redirect: '/market'
     }
   ]
 })
